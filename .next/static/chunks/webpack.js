dc02338f39b928b909c32c9f9bb7ce8ffd4e75ef
wambua.joseph24 <wambua.joseph24@student.cuk.ac.ke>
2025-11-11 12:02:14 +0000

    New commit after rebase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22185714c610de05"; }
+/******/ 		__webpack_require__.h = function() { return "1679d30cd2a7722d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
